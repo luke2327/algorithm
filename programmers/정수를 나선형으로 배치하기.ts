1cbc6271c42ef40ba72f75fff61324ce39d42d6f
luke2327 <qwepoi8526@gmail.com>
2024-03-30 15:18:05 +0900

    스택 및 프로그래머 문제 솔루션 추가

--- programmers/정수를 나선형으로 배치하기.ts
+++ programmers/정수를 나선형으로 배치하기.ts
@@ -1,9 +1,29 @@
 function solution(n) {
-    const answer = Array(n).fill(null).map(() => []);
-    let side = n - 1;
+    let answer: any[] = Array(n).fill(null).map(() => []);
+    const dx = [1,0,-1,0];
+    const dy = [0,1,0,-1];
+
+    let [x, y] = [0, 0];
+    let direction = 0;
+
     for (let i = 1; i <= n*n; i++) {
-        console.log(i);
+        answer[y][x] = i;
+
+        const [nx, ny] = [x + dx[direction], y + dy[direction]];
+
+        if (nx < 0 || nx >= n || ny < 0 || ny >= n || answer[ny][nx]) {
+            direction = (direction + 1) % 4;
+            x += dx[direction];
+            y += dy[direction];
+
+            console.log('x, y', x, y)
+        } else {
+            console.log('nx, ny', nx, ny)
+            x = nx;
+            y = ny;
+        }
     }
+
     return answer;
 }
 
---
+++ programmers/평행.ts
@@ -0,0 +1,15 @@
+function solution(dots) {
+    const [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = dots;
+
+    console.log((x1 - x2) / (y1 - y2), (x3 - x4) / (y3 - y4))
+
+    if ((x1 - x2) / (y1 - y2) === (x3 - x4) / (y3 - y4)) {
+        return 1;
+    } else if ((x1 - x3) / (y1 - y3) === (x2 - x4) / (y2 - y4)) {
+        return 1;
+    } else if ((x1 - x4) / (y1 - y4) === (x2 - x3) / (y2 - y3)) {
+        return 1;
+    }
+}
+
+console.log(solution([[1, 4], [9, 2], [3, 8], [11, 6]]))
